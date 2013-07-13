c687d6e122db4eced2d4c647c51d3db85f127a5b
Jeremy Dagorn <jeremy.dagorn@prezi.com>
2013-07-13 12:34:30 +0200

    [ruby] more koans.

--- ruby-koans/triangle.rb
+++ ruby-koans/triangle.rb
@@ -14,7 +14,16 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  e1 = a == b
+  e2 = a == c
+
+  if e1 and e1 == e2
+  	return :equilateral
+  elsif e1 || e2 || b == c
+  	return :isosceles
+  else
+  	return :scalene
+  end 
 end
 
 # Error class used in part 2.  No need to change this code.
